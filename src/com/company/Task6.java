b5b2043b4b3674a6f2615c32265fba849644ac6d
sjuesju <sjuesju@yahoo.no>
2020-02-16 19:20:33 +0200

    second commit

--- src/com/company/Task6.java
+++ src/com/company/Task6.java
@@ -3,8 +3,10 @@ package com.company;
 public class Task6 {
     public static void main(String[] args) {
         String[] address = {"Иван Иванов", "България", "София", "ул. Незабравка 12", "1000"};
+        System.out.println(formatLetter(address));
+
     }
     public static String formatLetter(String[]address) {
-        return String.format("%s\n%s\n%s\n%s\n%s", address[0], address[1], address[2], address[3], address[4], address[5] );
+        return String.format("%s\n%s\n%s\n%s\n%s", address[0], address[1], address[2], address[3], address[4] );
     }
 }
